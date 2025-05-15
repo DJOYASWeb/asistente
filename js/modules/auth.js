41221f4e3a6ffb4eca8208085ec46dd349426cf6
DJOYASWeb <web.djoyas@gmail.com>
2025-05-15 14:37:13 -0400

    upd

--- js/modules/auth.js
+++ js/modules/auth.js
@@ -1,20 +1,5 @@
 // auth.js
 
-// Configuración Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyD6xqVEHb5eGrFr4cEu6y-OHxcpXjvybv4",
-  authDomain: "djoyas-asistente.firebaseapp.com",
-  projectId: "djoyas-asistente",
-  storageBucket: "djoyas-asistente.firebasestorage.app",
-  messagingSenderId: "990292345351",
-  appId: "1:990292345351:web:72ae605299387fa31c20a2",
-  measurementId: "G-9ZTSMFYFE1"
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
 // Validación de email
 function isEmailValid(email) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
