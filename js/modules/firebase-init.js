39751464cb256a3f9d95e93809978774218b0e1e
DJOYASWeb <web.djoyas@gmail.com>
2025-05-15 14:35:55 -0400

    upd

---
+++ js/modules/firebase-init.js
@@ -0,0 +1,14 @@
+// firebase-init.js
+const firebaseConfig = {
+  apiKey: "AIzaSyD6xqVEHb5eGrFr4cEu6y-OHxcpXjvybv4",
+  authDomain: "djoyas-asistente.firebaseapp.com",
+  projectId: "djoyas-asistente",
+  storageBucket: "djoyas-asistente.firebasestorage.app",
+  messagingSenderId: "990292345351",
+  appId: "1:990292345351:web:72ae605299387fa31c20a2",
+  measurementId: "G-9ZTSMFYFE1"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
