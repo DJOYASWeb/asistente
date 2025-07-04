8511800c39a8869712c2b01b3e3614fbc588b691
Matias WEB <web.djoyas@gmail.com>
2025-07-04 17:24:16 -0400

    aa

--- js/modules/tabs.js
+++ js/modules/tabs.js
@@ -1,13 +1,16 @@
 window.showTab = function(tab) {
-  const tabs = ['contenidos', 'recursos', 'ingreso', 'crear', 'redactar', 'calendario','constructor','recursos','carga','historial'];
+  const tabs = ['contenidos', 'recursos', 'ingreso', 'crear', 'redactar', 'calendario','constructor','carga','historial'];
   tabs.forEach(t => {
     const section = document.getElementById(t);
     const btn = document.getElementById(`btn${capitalize(t)}`);
     if (section) section.classList.toggle('d-none', t !== tab);
     if (btn) btn.classList.toggle('active', t === tab);
   });
-}
 
+  if (tab === 'recursos' && typeof cargarRecursos === "function") {
+    cargarRecursos();
+  }
+}
 
 function capitalize(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
