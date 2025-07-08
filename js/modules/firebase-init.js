b86f3092a5c31f2560d4b3c4d576ca1fc90b45a0
Matias WEB <web.djoyas@gmail.com>
2025-07-08 17:56:42 -0400

    a

--- js/modules/firebase-init.js
+++ js/modules/firebase-init.js
@@ -1,16 +1,20 @@
 // firebase-init.js
-const firebaseConfig = {
+
+// Configuración de tu proyecto Firebase
+var firebaseConfig = {
   apiKey: "AIzaSyD6xqVEHb5eGrFr4cEu6y-OHxcpXjvybv4",
   authDomain: "djoyas-asistente.firebaseapp.com",
   projectId: "djoyas-asistente",
-  storageBucket: "djoyas-asistente.firebasestorage.app",
+  storageBucket: "djoyas-asistente.appspot.com",
   messagingSenderId: "990292345351",
   appId: "1:990292345351:web:72ae605299387fa31c20a2",
   measurementId: "G-9ZTSMFYFE1"
 };
 
+// Inicializa Firebase solo si no está inicializado
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
+// Inicializa Firestore y la expone globalmente
 window.db = firebase.firestore();
